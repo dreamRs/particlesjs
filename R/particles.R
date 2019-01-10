440d882d9dfbd0427b63c44a4e72133976908e0b
pvictor <perrier.victor@gmail.com>
2019-01-10 16:32:44 +0100

    up namespace

--- R/particles.R
+++ R/particles.R
@@ -8,11 +8,12 @@
 #'  visit \url{https://vincentgarreau.com/particles.js/} to create one.
 #'
 #' @importFrom htmlwidgets createWidget
+#' @importFrom htmltools htmlDependency
 #'
 #' @export
 particles <- function(path_config = NULL) {
   if (is.null(path_config)) {
-    config <- htmltools::htmlDependency(
+    config <- htmlDependency(
       name = 'particles-config',
       version = '0.1.0',
       src = system.file('particles', package = 'shinyparticles'),
@@ -20,14 +21,14 @@ particles <- function(path_config = NULL) {
     )
   } else {
     path_config <- normalizePath(path = path_config, mustWork = TRUE)
-    config <- htmltools::htmlDependency(
+    config <- htmlDependency(
       name = 'particles-config',
       version = '0.1.0',
       src = dirname(path_config),
       attachment = list("config-json" = basename(path_config))
     )
   }
-  htmlwidgets::createWidget(
+  createWidget(
     name = 'particles',
     x = list(),
     width = NULL,
